a38a6793be380751120d7334ddcc4fb2eb7f8410
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-05-11 12:20:06 +0300

    primitive and String

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Output is: Hello World!
     }
+
+    //primitives
+
+    byte bytePrimitive = 127;
+    short shortPrimitive = 3419;
+    char charPrimitive = 'a';
+    int intPrimitive = 32345434;
+    long longPrimitive = 2131234323;
+    float floatPrimitive = 123.123f;
+    double doublePrimitive = 123.1231231343454564;
+
+    String string = "This is String";
 }
