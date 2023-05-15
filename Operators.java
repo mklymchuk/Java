852977532619c3a85356c8fa45d586b7f2bb4f2d
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-05-15 18:07:21 +0300

    Operators, classes

---
+++ Operators.java
@@ -0,0 +1,7 @@
+public class Operators {
+
+    public static void main(String[] args) {
+        boolean rap = true &&! (false || true);
+        System.out.println(rap);    
+    }
+}
---
+++ Switch.java
@@ -0,0 +1,37 @@
+public class Switch {
+    public static void main(String[] args) {
+        String course = "Math";
+
+        switch(course) {
+            case "Sciense":
+            break;
+            case "Geology":
+            break;
+            case "Math":
+            break;
+            case "Gymnastic":
+            break;
+            default:
+            System.out.println("No course");
+        }
+
+        int fruit = 5;
+
+        switch(fruit) {
+            case 1:
+            System.out.println("Banana");
+            break;
+            case 2:
+            System.out.println("Kiwi");
+            break;
+            case 3:
+            System.out.println("Apple");
+            break;
+            case 4:
+            System.out.println("Orange");
+            break;
+            case 5:
+            System.out.println("Pomegrante");
+        }
+    }
+}
