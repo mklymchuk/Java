1d4174a92c846f68b573d266b270d3a2da7e7e38
mklymchuk <m.klymchuk@student.po.edu.pl>
2023-05-17 22:19:07 +0300

    enum

---
+++ LightTrafficSignal.java
@@ -0,0 +1,15 @@
+enum LightTrafficSignal {
+    RED ("halt"), GREEN("go"), ORANGE("slower");
+    private String move;
+    public String getMove() {
+        return this.move;
+    }
+    private LightTrafficSignal (String move) {
+        this.move = move;
+    }
+}
+
+LightTrafficSignal signal = LightTrafficSignal.valueOf("RED");
+signal = LightTrafficSignal.valueOf("GREEN");
+signal = LightTrafficSignal.valueOf("ORANGE");
+
