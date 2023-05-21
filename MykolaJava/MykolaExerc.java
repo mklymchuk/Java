426b60328b5af0b0e5ffd04d49c585f6cc62f993
mklymchuk <mklymchuk1994@gmail.com>
2023-05-21 16:26:00 +0300

    Mykolas java exc

---
+++ MykolaJava/MykolaExerc.java
@@ -0,0 +1,9 @@
+package MykolaJava;
+
+public class MykolaExerc {
+
+    //simple Hello World method
+    public void printHW () {
+        System.out.println("Mykolas hello world!");
+    }
+}
---
+++ MykolaMain.java
@@ -0,0 +1,8 @@
+import MykolaJava.*;
+//my package for testing skills in java
+public class MykolaMain {
+    public static void main(String[] args) {
+        MykolaExerc newMykolaExerc = new MykolaExerc();
+        newMykolaExerc.printHW();
+    }
+}
